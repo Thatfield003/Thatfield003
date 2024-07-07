1616d18c61d473e5df0c309016eb368830b32393
Tad Hatfield <thatfield003@gmail.com>
2024-07-07 01:42:41 -0500

    Compiled Changes - Sun Jul  7 01:42:40 CDT 2024 | No image optimization was used

--- ghost-updater.sh
+++ ghost-updater.sh
@@ -60,6 +60,10 @@ while getopts ":o:" opt; do
       echo "Invalid option: -$OPTARG"
       exit 1
       ;;
+    :)
+      echo "Option -$OPTARG requires an argument."
+      exit 1
+      ;;
   esac
 done
 
